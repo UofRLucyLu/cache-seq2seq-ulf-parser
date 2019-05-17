929009b7990a30da8ead1913c8a21d811646c824
Gene Louis Kim <gkim21@cs.rochester.edu>
2019-05-17 10:12:02 -0400

    Fixed stanford corenlp download script.

--- download_stanford_corenlp.sh
+++ download_stanford_corenlp.sh
@@ -1,6 +1,6 @@
 
 cd tools
-#wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip
+wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip
 module load unzip
 unzip stanford-corenlp-full-2018-10-05.zip
 rm stanford-corenlp-full-2018-10-05.zip
